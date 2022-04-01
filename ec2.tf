9ca6bb3d9da7f815d2e0122fc80aa4223ff8863f
amitvm <53211276+amitvm@users.noreply.github.com>
2022-04-01 20:29:13 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -4,9 +4,6 @@ resource "aws_key_pair" "deployer" {
 }
 
 resource "aws_instance" "ec2" {
-    depends_on = [
-    aws_security_group.web
-  ]
   ami = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
   key_name                    = "${var.name}-key"
