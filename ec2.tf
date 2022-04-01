a23aed46335ddd6aa48341e07ba4f2c72efca357
amitvm <53211276+amitvm@users.noreply.github.com>
2022-04-01 20:35:19 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "ec2" {
   ami = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
   key_name                    = "${var.name}-key"
-  vpc_security_group_ids      = [aws_security_group.web.id]
+  vpc_security_group_ids      = [var.web_sec_id]
   associate_public_ip_address = true
   subnet_id                   = var.subnet_id
 
