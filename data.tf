2eab5c0a2d2ab1fc7053a76d2638096c981816ae
amitvm <53211276+amitvm@users.noreply.github.com>
2022-04-01 22:28:21 +0530

    Update data.tf

--- data.tf
+++ data.tf
@@ -13,3 +13,14 @@ data "aws_ami" "ubuntu" {
 
   owners = ["099720109477"] # Canonical
 }
+
+data "template_file" "init" {
+  template = "${file(var.userdata)}"
+
+  vars = {
+    WORDPRESS_DB_NAME = "${var.db_name}"
+    WORDPRESS_DB_USER = "${var.db_user}"
+    WORDPRESS_DB_PASSWORD = "${var.db_pwd}"
+    WORDPRESS_DB_HOST = "${var.db_host}"
+  }
+}
